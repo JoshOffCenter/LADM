778107710bd506a6a46226a73213837dcdf2c389
Chance Daniel <ChanceDaniel@Chances-MacBook-Pro.local>
2015-04-29 22:06:16 -0700

    UI Data Picker Fully Functional

---
+++ LADM/SelectCityView.swift
@@ -0,0 +1,18 @@
+//
+//  SelectCityView.swift
+//  LADM
+//
+//  Created by Chance Daniel on 4/29/15.
+//  Copyright (c) 2015 MoonBase. All rights reserved.
+//
+
+import UIKit
+
+class SelectCityView: UIView {
+   
+   @IBOutlet weak var cityPickerView: UIPickerView!
+   
+//   let blurEffect = UIBlurEffect(style: .Light)
+//   let blurView = UIVisualEffectView(effect: blurEffect)
+   
+}
